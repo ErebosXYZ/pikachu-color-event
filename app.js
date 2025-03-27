fe98abf5b19e3cf4340354c42724f9ed76470f03
ErebosXYZ <robertmaynou@gmail.com>
2025-03-27 21:01:58 +0100

    first commit

--- app.js
+++ app.js
@@ -5,9 +5,19 @@
 document.querySelector('#first').addEventListener('click', updateBgColor);
 document.querySelector('#second').addEventListener('click', updateBgColor);
 document.querySelector('#third').addEventListener('click', updateBgColor);
-
+document.querySelector('#fourth').addEventListener('click', updateBgColor);
 // MODIFICAR A PARTIR DE AQUÍ 
 
-function updateBgColor(event) {
+// La funció updateBgColor canvia el color de fons del pikachu segons el botó que apreti
+// El data-type dels elements html tenen un color que és una strng. per tant canviant el background color al style de la imatge per la string del data color potser podem fer-ho.
+// Quan cliqui #first el background color ha de canviar a red, quan sigui #second blue i així amb tots.
+// Provar estructura condicional. No funciona.
+let pikachu = document.querySelector("img");
+console.log(pikachu);
 
+function updateBgColor(event) {
+   let clickedButton = event.target;
+   let color = clickedButton.getAttribute("data-color");
+   pikachu.style.backgroundColor = color;
+    
 }
